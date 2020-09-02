177dbbe1836defdff6d7fd110fe7dcb73f06fc03
mkn1495 <manojnayakbellala@live.com>
2020-09-02 23:00:43 +0530

    added ListView FloatingBtn SnackBar Sec_2_9

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,50 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
-      home: Home(),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Long List"),
+        ),
+        body: getListView(),
+        floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () => print("FAB clicked"),
+          tooltip: "Add one more item",
+          elevation: 5,
+        ),
+      ),
     );
   }
+}
+
+void showSnackBar(BuildContext context, String item) {
+  var snackBar = SnackBar(
+    content: Text("You just tapped $item"),
+    action: SnackBarAction(
+      label: "UNDO",
+      onPressed: () => print("Performing dummy undo operation"),
+    ),
+  );
+
+  Scaffold.of(context).showSnackBar(snackBar);
+}
+
+List<String> getListElements() {
+  var items = List<String>.generate(1000, (index) => "Item $index");
+  return items;
+}
+
+Widget getListView() {
+  var listItems = getListElements();
+
+  var listView = ListView.builder(
+    itemBuilder: (context, index) {
+      return ListTile(
+        leading: Icon(Icons.laptop),
+        title: Text(listItems[index]),
+        onTap: () => showSnackBar(context, listItems[index]),
+      );
+    },
+  );
+  return listView;
 }
